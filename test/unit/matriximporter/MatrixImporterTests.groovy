b59c6cfc3e811b5baee27a4e53f5361ae5c04391
vrclvrncknm <s.h.sikkema@gmail.com>
2011-08-09 12:48:37 +0000

    - changed Reader to Parser consistently - using java.io.Reader instances instead of inputStreams to make sure all inputStreams can be reset - added a check in ExcelParser to stop when 10 or more formulas could not be interpreted (in order to save HUGE amount of time with catching exceptions) - added xls test - version -> 0.2.0

--- test/unit/matriximporter/MatrixImporterTests.groovy
+++ test/unit/matriximporter/MatrixImporterTests.groovy
@@ -1,8 +1,9 @@
 package matriximporter
 
 import grails.test.GrailsUnitTestCase
-import org.dbxp.matriximporter.CsvReader
-import org.dbxp.matriximporter.ExcelReader
+import org.dbxp.matriximporter.CsvParser
+import org.dbxp.matriximporter.ExcelParser
+import org.dbxp.matriximporter.MatrixImporter
 
 class MatrixImporterTests extends GrailsUnitTestCase {
     protected void setUp() {
@@ -13,30 +14,66 @@ class MatrixImporterTests extends GrailsUnitTestCase {
         super.tearDown()
     }
 
-    void testMockPPSH() {
-        def excelReader = new ExcelReader()
+    void testMockPPSHXLSX() {
+        def excelReader = new ExcelParser()
 
-        def matrix = excelReader.parse(new File('test_data/mock_PPSH.xlsx'))
+        def (matrix, parseInfo) = excelReader.parse(new File('test_data/mock_PPSH.xlsx'))
 
-        assert matrix.size == 14
+        assert matrix.size() == 14
         assert matrix[0].size == 154
     }
 
-    void testMockDiogenesEXCEL() {
-        def excelReader = new ExcelReader()
+    void testMockDiogenesXLSX() {
+        def excelReader = new ExcelParser()
 
-        def matrix = excelReader.parse(new File('test_data/DiogenesMockData.xlsx'))
+        def (matrix, parseInfo) = excelReader.parse(new File('test_data/DiogenesMockData.xlsx'))
 
-        assert matrix.size == 1986
+        assert matrix.size() == 1986
+        assert matrix[0].size == 163
+    }
+
+    void testMockDiogenesXLS() {
+        def excelReader = new ExcelParser()
+
+        def (matrix, parseInfo) = excelReader.parse(new File('test_data/DiogenesMockData.xls'))
+
+        assert matrix.size() == 1986
         assert matrix[0].size == 163
     }
 
     void testMockDiogenesTABDELIMITED() {
-        def csvReader = new CsvReader()
+        def csvReader = new CsvParser()
 
-        def matrix = csvReader.parse(new File('test_data/DiogenesMockData.txt'))
+        def (matrix, parseInfo) = csvReader.parse(new File('test_data/DiogenesMockData.txt'))
 
-        assert matrix.size == 1986
+        assert matrix.size() == 1986
         assert matrix[0].size == 163
+    }
+
+    void testMatrixImporterInputStream() {
+
+        def matrixImporterInstance = MatrixImporter.instance
+
+        def fileInputStream = new FileInputStream('test_data/DiogenesMockData_mini.txt')
+
+        def (matrix, parseInfo) = matrixImporterInstance.importInputStream(fileInputStream)
+
+        fileInputStream.close()
+
+        assert matrix
+        assert parseInfo
+
+    }
+
+    void testMatrixImporterString() {
+
+        def matrixImporterInstance = MatrixImporter.instance
+
+        def s = "1,2,3,2,311,\n,3,53,23"
+
+        def matrix = matrixImporterInstance.importString(s, [delimiter: ','])
+
+        assert matrix == [['1','2','3','2','311'],['','3','53','23']]
+
     }
 }

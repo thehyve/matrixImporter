45c6d10434ee17da16124f65a685f42a90b7d0e4
roberthorlings <robert@thehyve.nl>
2011-09-12 11:32:27 +0000

    Removed unnecessary println statements

--- src/groovy/org/dbxp/matriximporter/MatrixImporter.groovy
+++ src/groovy/org/dbxp/matriximporter/MatrixImporter.groovy
@@ -4,29 +4,29 @@ import org.apache.commons.logging.LogFactory
 
 /**
  * Singleton class to access the import methods for matrix imports. Use the importFile method for reading files.
- *
+ * 
  *  	MatrixImporter.getInstance().importFile( new File( '/tmp/temporaryfile' ) )
  *
  *  Also available are: 'importString', 'importByteArray', and 'importInputStream'.
- *
+ *  
  *  Currently, excel files (.xls and .xlsx) and csv files (comma, tab or semicolon delimited) are supported.
- *  You can use the register() and unregister() methods to add or remove parsers for specific file types
- *
- *  	// Create a parser implementing the MatrixParser interface. Also implemented the required methods
- *  	class MyTypeParser implements MatrixParser { ... }
- *
- *  	// Register a parser for your own filetype
- *  	def myTypeParser = new MyTypeParser()
- *  	MatrixImporter.getInstance().register( myTypeParser )
- *
+ *  You can use the register() and unregister() methods to add or remove readers for specific file types
+ *  
+ *  	// Create a reader implementing the MatrixReader interface. Also implemented the required methods	
+ *  	class MyTypeReader implements MatrixReader { ... }
+ *  	
+ *  	// Register a reader for your own filetype	
+ *  	def myTypeReader = new MyTypeReader()
+ *  	MatrixImporter.getInstance().register( myTypeReader )
+ *  
  *  	// Import the file
  *  	MatrixImporter.getInstance().importFile( new File( '/tmp/myTypeFile' ) )
- *
+ *  
  *  	// If needed
- *  	MatrixImporter.getInstance().unregister( MyTypeParser.class )
+ *  	MatrixImporter.getInstance().unregister( MyTypeReader.class )
  *  	// or
- *  	MatrixImporter.getInstance().unregister( myTypeParser )
- *
+ *  	MatrixImporter.getInstance().unregister( myTypeReader )
+ *  
  * @author Robert Horlings
  *
  */
@@ -37,8 +37,8 @@ class MatrixImporter {
 	// Singleton instance
 	private static MatrixImporter _instance = null
 
-	// List of registered parsers
-	private List<MatrixParser> parsers = []
+	// List of registered readers
+	private List<MatrixReader> readers = []
 
     /**
      *
@@ -71,87 +71,95 @@ class MatrixImporter {
     }
 
 	/**
-	 * Imports a file using an existing MatrixParser. If no parser is found that
+	 * Imports a file using an existing MatrixReader. If no reader is found that
 	 * is able to parse the file, null is returned.
 	 * @param file	File to read
-     * @param hints	Map with hints for the parser. The value corresponding to the key 'fileName' might be used
-     *              by the parsers to determine whether they can parse the file.
+     * @param hints	Map with hints for the reader. The value corresponding to the key 'fileName' might be used
+     *              by the readers to determine whether they can parse the file.
      *              Might also include keys like 'startRow', 'endRow' and 'sheet'.
-     * 				Parsers implementing this interface may or may not listen to the hints given. See the documentation
+     * 				Readers implementing this interface may or may not listen to the hints given. See the documentation
      * 				of different implementing classes.
 	 * @return		Two-dimensional data matrix with the contents of the file. The matrix has the structure:
-	 * 				[
+	 * 				[ 
 	 * 					[ 1, 3, 5 ] // First line
 	 * 					[ 9, 1, 2 ] // Second line
 	 * 				]
 	 */
-	public importInputStream( InputStream inputStream, Map hints = [:], Boolean returnInfo = false ) {
+	public importInputStream( InputStream inputStream ) {
+        importInputStream( inputStream, [:], false)
+    }
+    public importInputStream( InputStream inputStream, Map hints ) {
+            importInputStream( inputStream, hints, false)
+    }
+	public importInputStream( InputStream inputStream, Map hints, Boolean returnInfo) {
+		// Set a mark on the beginning of this inputstream, in order
+		// to be able to reset this stream when start reading with a new reader
+		if( inputStream.markSupported() )
+			inputStream.mark( Integer.MAX_VALUE );
 
-        def bis = new BufferedInputStream(inputStream)
-
-        bis.mark(MatrixParser.readAheadLimit)
-
-        for( parser in parsers ) {
-			if( parser.canParse( hints ) ) {
+		// Loop through all registered readers, and parse the file using
+		// the first reader that is able to parse the file.
+		for( reader in readers ) {
+			if( reader.canParse( hints ) ) {
                 def matrix, parseInfo
 
                 try {
-                    bis.reset()
-
-                    (matrix, parseInfo) = parser.parse( bis, hints )
+                    // Reset inputStream, since it may have been used by other readers
+					if( inputStream.markSupported() ) {
+						inputStream.reset();
+					}
+                    
+                    (matrix, parseInfo) = reader.parse( inputStream, hints )
                 } catch (e) {
-                    // we take it an exception means this parser was unable to parse
+                    // we take it an exception means this reader was unable to parse
                     // the input.
-
-                    println 'Unable to parse using parser: ' + parser.class + ' with description: ' + parser.description + '.'
-                    e.printStackTrace();
-                    log.info('Unable to parse using parser: ' + parser.class + ' with description: ' + parser.description + '.', e)
+                    log.info('Unable to parse using reader: ' + reader.class + ' with description: ' + reader.description + '.', e)
                 }
 
                 // Only return the value if the file has been correctly parsed
                 // (i.e. the structure != null). Otherwise, we should try to parse
-                // the file using another parser (if applicable)
+                // the file using another reader (if applicable)
                 if( matrix ) {
-                    return returnInfo ? [matrix, [parserClassName: parser.class.simpleName] + parseInfo] : matrix
+                    return returnInfo ? [matrix, [readerClassName: reader.class.simpleName] + parseInfo] : matrix
                 }
 			}
 		}
 
 		// If parsing didn't work out, return null
 		returnInfo ? [null, null] : null
-    }
-
-	/**
-	 * Registers a parser with the importer, so the parser can be used to parse files
-	 * @param c	Object that implements MatrixParser
-	 */
-	public void registerParser( MatrixParser c ) {
-		if( c instanceof MatrixParser && !( c in parsers ) )
-		parsers << c
 	}
 
 	/**
-	 * Removes a parser from the importer, so the parser will not be used to parse files
-	 * @param c		Class that implements MatrixParser
+	 * Registers a reader with the importer, so the reader can be used to parse files
+	 * @param c	Object that implements MatrixReader
 	 */
-	public void unregisterParser( MatrixParser c ) {
-		parsers = parsers.findAll { !( it.is( c ) ) }
+	public void registerReader( MatrixReader c ) {
+		if( c instanceof MatrixReader && !( c in readers ) )
+		readers << c
 	}
 
 	/**
-	 * Removes a parser from the importer, so the parser will not be used to parse files
-	 * @param c		Class that implements MatrixParser
+	 * Removes a reader from the importer, so the reader will not be used to parse files
+	 * @param c		Class that implements MatrixReader
 	 */
-	public void unregisterParser( Class c ) {
-		parsers = parsers.findAll { !( it.class == c ) }
+	public void unregisterReader( MatrixReader c ) {
+		readers = readers.findAll { !( it.is( c ) ) }
 	}
 
 	/**
-	 * Returns a list of parsers that have been registered
-	 * @return	List with MatrixParser objects
+	 * Removes a reader from the importer, so the reader will not be used to parse files
+	 * @param c		Class that implements MatrixReader 
 	 */
-	public List<MatrixParser> getParsers() {
-		return [] + parsers
+	public void unregisterReader( Class c ) {
+		readers = readers.findAll { !( it.class == c ) }
+	}
+	
+	/**
+	 * Returns a list of readers that have been registered
+	 * @return	List with MatrixReader objects
+	 */
+	public List<MatrixReader> getReaders() {
+		return [] + readers
 	}
 
 	/**
@@ -164,11 +172,12 @@ class MatrixImporter {
 
 		return _instance
 	}
-
+	
+	
 	// Private constructor in order to facilitate the singleton pattern
 	private MatrixImporter() {
-		registerParser( new ExcelParser() )
-		registerParser( new CsvParser() )
+		registerReader( new ExcelReader() )
+		registerReader( new CsvReader() )
 	}
 
 }
